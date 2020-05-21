5fc8e1246448b6f8bfb9dbeccaa84d3c45e7c40b
sonusathyadas <sonusathyadas@hotmail.com>
2020-05-21 22:27:42 +0530

    initial

---
+++ ServiceBusQueue/Program.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Core;
+using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServiceBusQueue
+{
+    class Program
+    {
+        static string connectionString = "Endpoint=sb://sonu-sbus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=/BSeoT8+6x5ymBvxaRfxYleubnvdpiGffJaowOLKeMg=";
+        static string queueName = "items";
+
+        static async Task SendMessagesAsync(string connectionString, string queueName)
+        {
+            var sender = new MessageSender(connectionString, queueName);
+
+            dynamic data = new[]
+            {
+                new {name = "Einstein", firstName = "Albert"},
+                new {name = "Heisenberg", firstName = "Werner"},
+                new {name = "Curie", firstName = "Marie"},
+                new {name = "Hawking", firstName = "Steven"},
+                new {name = "Newton", firstName = "Isaac"},
+                new {name = "Bohr", firstName = "Niels"},
+                new {name = "Faraday", firstName = "Michael"},
+                new {name = "Galilei", firstName = "Galileo"},
+                new {name = "Kepler", firstName = "Johannes"},
+                new {name = "Kopernikus", firstName = "Nikolaus"}
+            };
+
+
+            for (int i = 1; i <= data.Length; i++)
+            {
+                var message = new Message(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(data[i])))
+                {
+                    ContentType = "application/json",
+                    Label = "Scientist",
+                    MessageId = i.ToString(),
+                    TimeToLive = TimeSpan.FromMinutes(2)
+                };
+
+                await sender.SendAsync(message);
+                lock (Console.Out)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Message sent: Id = {0}", message.MessageId);
+                    Console.ResetColor();
+                }
+            }
+        }
+
+        static async Task ReceiveMessagesAsync(string connectionString, string queueName, CancellationToken cancellationToken)
+        {
+            var receiver = new MessageReceiver(connectionString, queueName, ReceiveMode.PeekLock);
+
+
+            var doneReceiving = new TaskCompletionSource<bool>();
+            // close the receiver and factory when the CancellationToken fires 
+            cancellationToken.Register(
+                async () =>
+                {
+                    await receiver.CloseAsync();
+                    doneReceiving.SetResult(true);
+                });
+
+            // register the RegisterMessageHandler callback
+            receiver.RegisterMessageHandler(
+                async (message, cancellationToken1) =>
+                {
+                    if (message.Label != null &&
+                        message.ContentType != null &&
+                        message.Label.Equals("Scientist", StringComparison.InvariantCultureIgnoreCase) &&
+                        message.ContentType.Equals("application/json", StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        var body = message.Body;
+
+                        dynamic scientist = JsonConvert.DeserializeObject(Encoding.UTF8.GetString(body));
+
+                        lock (Console.Out)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            Console.WriteLine(
+                                "\tMessage received: \n\t\tMessageId = {0}, \n\t\tSequenceNumber = {1}, \n\t\tEnqueuedTimeUtc = {2}," +
+                                "\n\t\tExpiresAtUtc = {5}, \n\t\tContentType = \"{3}\", \n\t\tSize = {4},  \n\t\tContent: [ firstName = {6}, name = {7} ]",
+                                message.MessageId,
+                                message.SystemProperties.SequenceNumber,
+                                message.SystemProperties.EnqueuedTimeUtc,
+                                message.ContentType,
+                                message.Size,
+                                message.ExpiresAtUtc,
+                                scientist.firstName,
+                                scientist.name);
+                            Console.ResetColor();
+                        }
+                        await receiver.CompleteAsync(message.SystemProperties.LockToken);
+                    }
+                    else
+                    {
+                        await receiver.DeadLetterAsync(message.SystemProperties.LockToken); //, "ProcessingError", "Don't know what to do with this message");
+                    }
+                },
+                new MessageHandlerOptions((e) => LogMessageHandlerException(e)) { AutoComplete = false, MaxConcurrentCalls = 1 });
+
+            await doneReceiving.Task;
+        }
+
+        private static Task LogMessageHandlerException(ExceptionReceivedEventArgs e)
+        {
+            Console.WriteLine("Exception: \"{0}\" {0}", e.Exception.Message, e.ExceptionReceivedContext.EntityPath);
+            return Task.CompletedTask;
+        }
+
+        public static async Task Run(string connectionString, string queueName)
+        {
+            var cts = new CancellationTokenSource();
+
+            var sendTask = SendMessagesAsync(connectionString, queueName);
+            var receiveTask = ReceiveMessagesAsync(connectionString, queueName, cts.Token);
+
+            await Task.WhenAll(
+                 Task.WhenAny(
+                    Task.Run(() => Console.ReadKey()),
+                    Task.Delay(TimeSpan.FromSeconds(10))
+                ).ContinueWith((t) => cts.Cancel()),
+                sendTask,
+                receiveTask);
+        }
+
+        public static int Main(string[] args)
+        {
+
+            try
+            {
+                Run(connectionString,queueName).GetAwaiter().GetResult();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                //Console.WriteLine(e.ToString());
+                return 1;
+            }
+            return 0;
+        }
+    }
+
+
+}
